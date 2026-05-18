cb3a355a1be262b617d3f0252a7f6eac675b2e67
vanshika898 <vanshikanigam8989@gmail.com>
2026-05-19 01:38:06 +0530

    now databse connected successfully and Orm created

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,14 @@
 const express  = require("express");
 const app = express();
 require('dotenv').config();
+const db = require('./config/Database');
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
   res.send("hii my name is vanshika")
 })
+
 app.listen(PORT,()=>{
   console.log(`App listen on http://localhost:${PORT}`);
 })
